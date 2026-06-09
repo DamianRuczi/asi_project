9b318b8435066aa229ec5dc6c797f92a832bf828
Damian Ruczynski <damian.m.ruczynski@gmail.com>
2026-06-09 10:38:58 +0200

    initialize asi project structure

---
+++ models/model.py
@@ -0,0 +1,13 @@
+"""
+4. Udoskonalanie modelu 
+Implementacja systemu śledzenia eksperymentów:
+◦ np. MLflow / Weights & Biases
+• Wykorzystanie AutoML:
+◦ np. Autogluon, TPOT, PyCaret
+• Inżynieria cech:
+◦ selekcja cech
+◦ transformacje
+• Strojenie hiperparametrów:
+◦ Grid Search / Random Search / Bayesian Optimization
+• Porównanie kilku modeli
+"""
---
+++ models/pipeline.py
@@ -0,0 +1,5 @@
+"""
+3. Struktura projektu i pipeline 
+Implementacja pipeline’u ML z użyciem:
+◦ Kedro (lub alternatyw)
+"""
